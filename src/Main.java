3a9cfe34d0f0ac44078eb65ded900431571a4d45
beratuzd <230316043@ogr.cbu.edu.tr>
2025-12-24 23:21:38 +0300

    Integrate Student ID as random seed.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     private static final long STUDENT_ID = 230316043L;
-
+    Random rand = new Random(STUDENT_ID);
     public static void main(String[] args) {
         HospitalSystem sys = new HospitalSystem(STUDENT_ID);
         ConsoleUI ui = new ConsoleUI(sys);
